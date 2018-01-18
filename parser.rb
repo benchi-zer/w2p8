4e6cf85faa9bc7ffe9996c80e95b4aec94a7e50a
benchi-zer <eliebenchi@gmail.com>
2018-01-18 16:57:01 +0100

    first commit

---
+++ parser.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+def get_town_url
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+  page.css('table.Style20 a') .each do |node|
+  node['href'] = node['href'][1..-1]
+  town_url = "http://annuaire-des-mairies.com" + node['href']
+  puts town_url
+  end
+
+
+# def get_town_email
+#  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/eaubonne.html"))
+#  page.css('.style27 p.Style22') .each do |node|
+#   if node.text.include?"@"
+#     puts node.text
+#   end
+#  end
+# end
+# puts get_town_email
+end
+puts get_town_url
